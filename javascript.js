629374f06edca65d0205523a3836af64dd95dbf8
Clay <claytonrrogers@gmail.com>
2022-09-22 15:16:28 -0400

    Edit javascript.js
    Added simple algorithm comments to get started.

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// ALGORITHM for rock, paper, scissors
+
+// computer randomly chooses rock, paper, or scissors
+// player inputs rock, paper, or scissors
+// choices are compared between player and computer
+// rock beats scissors
+// paper beats rock
+// scissors beats paper
+// output a message saying what player chose and what computer chose
+// output a message that says if player wins, loses, or ties
+// play 5 times total (best of 5)
+// keep track of wins, losses, ties for player and computer
+// wins are compared between player and computer
