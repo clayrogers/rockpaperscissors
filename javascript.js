07f2b42537cfcbcb59b280bece99467582ee57a8
Clay <claytonrrogers@gmail.com>
2022-09-22 15:48:39 -0400

    Add function to javascript.js
    Function to play a round of rock, paper,
    scissors. Compares the choices of the player
    and the computer and returns a message.
    Converts strings to lowercase first for easier
    comparison whether the user enters it lowercase
    or uppercase.

--- javascript.js
+++ javascript.js
@@ -7,11 +7,23 @@ function getComputerChoice() {
     return computerOptions[randomChoice];
 }
 
-// player inputs rock, paper, or scissors
 // choices are compared between player and computer
 // rock beats scissors
 // paper beats rock
 // scissors beats paper
+function playRound(playerSelection, computerSelection) {
+    playerLower = playerSelection.toLowerCase();
+    computerLower = computerSelection.toLowerCase();
+    if (playerLower === 'rock' && computerLower === 'scissors' || playerLower === 'paper' && computerLower === 'rock' || playerLower === 'scissors' && computerLower === 'paper') {
+        return `You Win! ${playerSelection} beats ${computerSelection}!`;
+    } else if (playerLower == computerLower) {
+        return `It's a Tie! You both chose ${computerSelection}`;
+    } else {
+        return `You Lose! ${playerSelection} loses to ${computerSelection}`;
+    }
+}
+
+// player inputs rock, paper, or scissors
 // output a message saying what player chose and what computer chose
 // output a message that says if player wins, loses, or ties
 // play 5 times total (best of 5)
